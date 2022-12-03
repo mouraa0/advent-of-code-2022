fc6539e788687f3dfb43dd98b9a73a3535db13a4
mouraa0 <jpcmouraa@gmail.com>
2022-12-02 22:34:54 -0300

    day two puzzles

---
+++ day-two/first_puzzle.py
@@ -0,0 +1,53 @@
+# A - rock, B - paper, C - scissors
+# X - rock, Y - paper, Z - scissors
+
+# Points by choice:
+# 3 - scissors
+# 2 - paper
+# 1 - rock
+
+# Points by result:
+# 6 - win
+# 3 - draw
+# 0 - loss
+
+# The score of each game is the sum of the choice points and the result points
+# Total score according to the given input
+
+def get_choice_points(players_choice):
+    choices_dict = {
+        'X': 1,
+        'Y': 2,
+        'Z': 3,
+    }
+
+    return choices_dict.get(players_choice)
+
+
+def get_result_points(play_line):
+    result_dict = {
+        'AX': 3,
+        'AY': 6,
+        'AZ': 0,
+        'BX': 0,
+        'BY': 3,
+        'BZ': 6,
+        'CX': 6,
+        'CY': 0,
+        'CZ': 3,
+    }
+
+    return result_dict.get(play_line.replace(' ','')) + get_choice_points(play_line[2])
+
+def main():
+    archive_name = 'day-two/list.txt'
+    archive_data = open(archive_name, 'r')
+    score = 0
+
+    while line := archive_data.readline():
+        score += get_result_points(line.replace('\n', ''))
+    
+    print(score)
+
+
+main()
---
+++ day-two/second_puzzle.py
@@ -0,0 +1,89 @@
+# A - rock, B - paper, C - scissors
+# X - lose, Y - draw, Z - win
+
+# Points by choice:
+# 3 - scissors
+# 2 - paper
+# 1 - rock
+
+# Points by result:
+# 6 - win
+# 3 - draw
+# 0 - loss
+
+# The score of each game is the sum of the choice points and the result points
+# Total score according to the given input
+
+def get_determined_result(result_letter):
+    determined_result_dict = {
+        'X': 'lose',
+        'Y': 'draw',
+        'Z': 'win',
+    }
+
+    return determined_result_dict.get(result_letter)
+
+
+def get_choice_by_determined_result(choice, result):
+    choice_by_determined_result_dict = {
+        'A': {
+            'lose': 'C',
+            'draw': 'A',
+            'win': 'B',
+        },
+        'B': {
+            'lose': 'A',
+            'draw': 'B',
+            'win': 'C',
+        },
+        'C': {
+            'lose': 'B',
+            'draw': 'C',
+            'win': 'A',
+        },
+    }
+
+    return choice_by_determined_result_dict.get(choice).get(result)
+
+
+def get_choice_points(players_choice):
+    choice_points_dict = {
+        'A': 1,
+        'B': 2,
+        'C': 3,
+    }
+
+    return choice_points_dict.get(players_choice)
+
+
+def get_results_points(result_str):
+    result_points_dict = {
+        'lose': 0,
+        'draw': 3,
+        'win': 6,
+    }
+
+    return result_points_dict.get(result_str)
+
+
+def get_play_score(play_line):
+    determined_result = get_determined_result(play_line[2])
+    result_points = get_results_points(determined_result)
+    choice_points = get_choice_points(get_choice_by_determined_result(play_line[0], determined_result))
+
+
+    return result_points + choice_points
+
+
+def main():
+    archive_name = 'day-two/list.txt'
+    archive_data = open(archive_name, 'r')
+    score = 0
+
+    while line := archive_data.readline():
+        score += get_play_score(line.replace('\n', ''))
+
+    print(score)
+
+
+main()
